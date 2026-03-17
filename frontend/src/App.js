ec4ea65d0e34cfaede599b5744406438edbb0435
Lordju <160488704+Lordju@users.noreply.github.com>
2026-03-17 11:35:48 +0000

    feat: Integrate React Router and add Navbar to main application layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <h1>African Media Review Platform</h1>
-    </div>
+    <Router>
+      {/* The Navbar sits outside the Routes so it always shows up */}
+      <div className="min-h-screen bg-gray-100">
+        <Navbar />
+
+        <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* We will build these pages in the next commits! */}
+            {/* <Route path="/movies" element={<Movies />} /> */}
+            {/* <Route path="/books" element={<Books />} /> */}
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
